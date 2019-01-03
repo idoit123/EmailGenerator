7ff18fb12d247ba37cb7864b3038755d49af1ebf
sivbraec <simon.vanbraeckel@ugent.be>
2019-01-03 20:43:04 +0100

    Add opening mailbox

--- ding.py
+++ ding.py
@@ -1,17 +1,35 @@
 #!/bin/python3
 
+
+
 import requests
 import pyperclip
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-d=requests.get("http://temp-mail.org/en/")
+d=requests.get("http://www.yopmail.com/en/email-generator.php")
 string=d.text
 
-prefix='id="mail" onclick="select(this);" data-original-title="Your temporary Email address" class="mail opentip" value="'
-suffix='" data-placement="bottom" title="" type="text" readonly>'
+prefix='onmouseup="this.select();" type="text" value="'
+suffix='&#64;yopmail.com"'
+mailsuffix="&#64;yopmail.com"
 
 startIndex=string.find(prefix) + len(prefix)
-secondIndex=string.find(suffix)
+endIndex=string.find(suffix) + len(mailsuffix)
 
-email=string[startIndex:secondIndex]
+email=string[startIndex:endIndex].replace("&#64;", "@")
 
+print(email)
 pyperclip.copy(email)
+
+driver = webdriver.Chrome()
+driver.get("http://www.yopmail.com/en/")
+
+inputElement = driver.find_element_by_id("login")
+inputElement.send_keys(email)
+
+#Now you can simulate hitting ENTER:
+inputElement.send_keys(Keys.ENTER)
+#or if it is a form you can submit:
+#inputElement.submit()
+
